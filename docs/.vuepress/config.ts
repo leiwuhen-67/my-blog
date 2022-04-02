e67c07aab0debe6d61f91bb1f778b56ff94e8fb8
leiwuhen-67 <2811401616@qq.com>
2022-04-02 17:40:03 +0800

    更新

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -107,7 +107,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 博主信息 (显示在首页侧边栏)
     blogger: {
-      avatar: '/img/avatar.jpeg',
+      avatar: 'https://leiwuhen-67.github.io/my-blog/img/avatar.jpeg',
       name: 'leiwuhen-67',
       slogan: '搬砖人',
     },
