622adf9579b1c23085ce85dd9255fd03311814e6
leiwuhen-67 <2811401616@qq.com>
2022-05-20 13:26:35 +0800

    更新

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -59,7 +59,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
             items: [
               {
                 text: '《Mock》笔记',
-                link: '/pages/2403dc/'
+                link: '/random/mock/'
               }, {
                 text: '《MySQL》笔记',
                 link: '/random/mysql/'
@@ -68,11 +68,13 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           }
         ]
       },
+      { text: '影音播放', link: '/movie/' },
+      { text: '收藏', link: '/collect/website/'},
       { text: '关于', link: '/pages/5699bd/' }
     ],
     sidebarDepth: 3, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     // logo: 'https://leiwuhen-67.github.io/images/avatar.jpg', // 导航栏logo
-    // repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: 'https://github.com/leiwuhen-67/my-blog', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
