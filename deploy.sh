c9cf391394fac5f576ba7f4051d6f921566c728f
leiwuhen-67 <2811401616@qq.com>
2024-07-02 11:06:45 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,18 @@ set -e
 
 # 生成静态文件
 npm run build
- 
+
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd ./dist
 
 echo 'www.jiangfeng.xyz' > CNAME
- 
+
 git init
 git add -A
 git commit -m 'deploy'
- 
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:leiwuhen-67/my-blog.git master:dev
- 
+
 cd -
-rm -rf docs/.vuepress/dist
+rm -rf ./dist
